71f23cdd851ff99d7153de573de993e4ff7ec249
Andrei <Andrei@HP2000>
2016-07-30 13:38:03 -1200

    Tema de casa din data de 22 iulie 2016

---
+++ JavaTraining/src/tema2/MyMessages.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema2;
+
+/**
+ *
+ * @author Andrei
+ */
+public class MyMessages {
+    public static void main(String[] args){
+        System.out.println("This is my first Java Program !"+ "\n" + "So Hello Java it is"+ "\n" + "Java is WORA");
+    }
+    
+}
